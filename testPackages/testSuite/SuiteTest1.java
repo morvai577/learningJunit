76ca6c89efb4d5268c983f9a585df72a258408e6
Vaibhav Mor <mor.vaibhav577@gmail.com>
2018-05-04 14:09:25 +1200

    Add test suite to execute multiple JUnit test classes simultaneously

---
+++ testPackages/testSuite/SuiteTest1.java
@@ -0,0 +1,14 @@
+package testSuite;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SuiteTest1 {
+
+	@Test
+	public void testPrintMessage() {
+		int num = 5;System.out.println("Suite Test1 is executing");
+		Assert.assertEquals(5, num);
+	}
+
+}
